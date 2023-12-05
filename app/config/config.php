b70d7ccf754de4c7264c3cae1f345a6fb737ffc8
Your Name <you@example.com>
2023-12-05 01:25:56 +0100

    login part1

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+    define("DB_HOST" , 'localhost');
+    define("DB_USER" , 'root');
+    define("DB_PASS" , '');
+    define("DB_NAME" , 'db_bank');
+
+// Redirect Funtion 
+function Redirect($url, $permanent = false)
+{
+    header('Location: ' . $url, true, $permanent ? 301 : 302);
+
+    exit();
+}
+
+
+
+?>
